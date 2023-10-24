a92f9c70d59c6ed2b302d16403002837cab7d302
Robin Sabhaya <robin47ppsv@gmail.com>
2023-10-24 11:25:08 +0530

    fix the bug

--- controllers/customer/loginController.js
+++ controllers/customer/loginController.js
@@ -1,26 +1,45 @@
-const registerModel = require('../../db/models/registerSchema');
+const registerModel = require("../../db/models/registerSchema");
 const SECRET_KEY = process.env.SECRET_KEY;
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const loginController = () => {
   return {
     getLogin(req, res) {
       return res.status(200).render("Login_Page");
     },
     async postLogin(req, res) {
-        const { Lemail, Lpassword } = req.body;
-        const data = await registerModel.findOne({ email: Lemail });
-        const hash = await bcrypt.compare(Lpassword, data.password);
-        if (Lemail === data.email && hash === true) {
-          const token = await jwt.sign({_id : data._id,user : data.name},SECRET_KEY);
-          res.cookie('login',token);
-          data.token = token;
-          await data.save();
-          req.session.user = {_id : data._id,user : data.name};
-          return res.status(301).redirect('/');
+      const { Lemail, Lpassword } = req.body;
+      const loginData = await registerModel.findOne({ email: Lemail });
+      if (loginData) {
+        const hash = await bcrypt.compare(Lpassword, loginData.password);
+        if (Lemail === loginData.email && hash === true) {
+          req.session.user = { _id: loginData._id, user: loginData.name };
+          return res.status(301).redirect("/");
+        } else {
+          req.flash("error", "Invalid email and password");
+          return res.redirect("/login");
         }
+      } else {
+        req.flash("error", "Invalid email and password");
+        return res.redirect("/login");
+      }
     },
-  }
+    async postLogout(req, res) {
+      try {
+        if (req.session.user) {
+          delete req.session.user;
+          return res.redirect("/login");
+        } else {
+          return res.redirect("/login");
+        }
+      } catch (err) {
+        return res.json({
+          status: 400,
+          message: err.message,
+        });
+      }
+    },
+  };
 };
 
 module.exports = loginController;
--- controllers/customer/registerController.js
+++ controllers/customer/registerController.js
@@ -1,33 +1,41 @@
-const bcrypt = require('bcryptjs');
-const registerModel = require('../../db/models/registerSchema');
-const registerController = () =>{
-    return {
-        getRegister(req,res){
-            return  res.status(200).render("Register_Page");
-        },
-        async postRegister(req,res){
-          const {name,email,password,cpassword} = req.body;
-                try {
-                  if (cpassword === password) {
-                    const hashPassword = await bcrypt.hash(password,10);
-                    const registerData = new registerModel({
-                      name : name,
-                      email : email,
-                      password : hashPassword,
-                    });
-                    // const token = await Web14data.jwtoken();
-                    // res.cookie("register", token);
-                    // Web14data.save().then(() => {
-                    //   res.status(200).json({ status: "sucess", code: "200" });
-                    // });
-                    await registerData.save();
-                    return res.status(302).redirect('/');
-                  } 
-                } catch (error) {
-                 return  res.send(error.message);
-                }
-              }
+const bcrypt = require("bcryptjs");
+const registerModel = require("../../db/models/registerSchema");
+const registerController = () => {
+  return {
+    getRegister(req, res) {
+      return res.status(200).render("Register_Page");
+    },
+    async postRegister(req, res) {
+      const { name, email, password, cpassword } = req.body;
+      try {
+        const registerData = await registerModel.exists({ email: email });
+        if (registerData) {
+          req.flash("credentials", "Email is already exist!!");
+          res.redirect("/register");
+        } else {
+          res.redirect("/register");
         }
-    }
+        if (cpassword === password) {
+          const hashPassword = await bcrypt.hash(password, 10);
+          const registerData = new registerModel({
+            name: name,
+            email: email,
+            password: hashPassword,
+          });
+          await registerData.save();
+          return res.status(302).redirect("/login");
+        } else {
+          req.flash("credentials", "Invalid password");
+          return res.redirect("/register");
+        }
+      } catch (err) {
+        return res.json({
+          status: 400,
+          message: err.message,
+        });
+      }
+    },
+  };
+};
 
 module.exports = registerController;
