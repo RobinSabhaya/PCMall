551f6439ed9bfff8996dbc3f3371fb5c4f4475f8
Robin Sabhaya <robin47ppsv@gmail.com>
2024-04-04 20:47:57 +0530

    Fix the issue of cache puppeteer

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const MONGODB_URL = process.env.MONGODB_URL;
-const conn = mongoose.connect(MONGODB_URL, {
+const conn = mongoose
+  .connect(MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+    strictQuery: false,
+  })
+  .then(() => {
     console.log("connection successfull");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 module.exports = conn;
