25a79150194f0df2cf4b95871dedc00ad2183d8e
Robin Sabhaya <robin47ppsv@gmail.com>
2024-05-14 20:46:08 +0530

    Create rating API for PCMall APP

--- controllers/customer/ratingController.js
+++ controllers/customer/ratingController.js
@@ -25,11 +25,14 @@ const ratingController = () => {
           rating,
           message,
         });
-        // return res.status(201).json({
-        //   status: 201,
-        //   message: "Rating created successfully",
-        // });
-        return res.status(302).redirect(`/singleproduct/${id}`); //Redirect to same page
+        if (req.xhr) {
+          return res.status(201).json({
+            status: 201,
+            message: "Rating created successfully",
+          });
+        } else {
+          return res.status(302).redirect(`/singleproduct/${id}`); //Redirect to same page
+        }
       } catch (err) {
         return res.status(400).json({
           status: 400,
